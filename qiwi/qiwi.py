a2d28edede69f304d19a288af0c356e53a1ae852
dani <dany@wisebits.com>
2020-04-18 00:12:40 +0300

    add

---
+++ qiwi/qiwi.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import telega_bot.settings as settings
+
+api_access_token = settings.api_qiwi_token
+mylogin = settings.qiwi_login
+
+
+# Профиль пользователя
+def get_profile(api_access_token):
+    s7 = requests.Session()
+    s7.headers['Accept'] = 'application/json'
+    s7.headers['authorization'] = 'Bearer ' + api_access_token
+    p = s7.get(
+        'https://edge.qiwi.com/person-profile/v1/profile/current?authInfoEnabled=true&contractInfoEnabled=true&userInfoEnabled=true')
+    return p.json()
+
+
+# Полная информация о профиле пользователя
+profile = get_profile(api_access_token)
+
+
+# print(profile)
+
+
+# История платежей - последние и следующие n платежей
+def payment_history_last(my_login, api_access_token, rows_num, next_TxnId, next_TxnDate):
+    s = requests.Session()
+    s.headers['authorization'] = 'Bearer ' + api_access_token
+    parameters = {'rows': rows_num, 'nextTxnId': next_TxnId, 'nextTxnDate': next_TxnDate}
+    h = s.get('https://edge.qiwi.com/payment-history/v2/persons/' + my_login + '/payments', params=parameters)
+    return h.json()
+
+
+# последние 100 платежей
+lastPayments = payment_history_last(mylogin, api_access_token, '4', '', '')
+
+
+def get_payment(count_payment):
+    lastPayments = payment_history_last(mylogin, api_access_token, count_payment, '', '')
+    data = lastPayments.get('data')[0]
+    comment = data.get('comment')
+    payment_sum = data.get('sum')
+    payment_sum_amount = payment_sum.get('amount')
+    return payment_sum_amount
+
+
+#
+print(lastPayments)
+#
+# print(type(lastPayments.get('data')))
+#
+# print(type(lastPayments.get('data')[3]))
+#
+# data = lastPayments.get('data')[1]
+# print(data)
+# comment = data.get('comment')
+# payment_sum = data.get('sum')
+# payment_sum_amount = payment_sum.get('amount')
+#
+# print(comment)
+# print(payment_sum_amount)
+if __name__ == "__main__":
+    get_payment(1)
